9b697da5c7ef8611d2139a05e6ad828447632e93
nksheoran <namkumshe@gmail.com>
2026-02-08 07:11:47 +0530

    Update base path in Vite configuration
    Changed base path for the Vite configuration to current folder.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Paper-Mock-/",
+  base: './', // Using the dot ./ tells the APK to look in the current folder
 })
