88be790b2200b1b340fef1448670ad6ec68667b7
nksheoran <namkumshe@gmail.com>
2026-02-08 07:16:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Using the dot ./ tells the APK to look in the current folder
+  base: './', // The dot-slash ./ is the magic fix for subfolder 404s
 })
