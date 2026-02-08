f7e055f753680104593d5b8852c26f3f7aa85eeb
nksheoran <namkumshe@gmail.com>
2026-02-08 07:33:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // The dot-slash ./ is the magic fix for subfolder 404s
+  base: '/', // Changed from ./ to / for Netlify root hosting
 })
